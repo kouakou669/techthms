2e30f0777891e4fcdfb086f8761ea25ff26bf3c3
THOMAS  SECONDE  DEVIS <kouamedjakiss766@gmail.com>
2025-04-20 11:18:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 const express = require('express');
 const app = express();
-__path = process.cwd(); // Récupère le chemin actuel du travail
+__path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-
-// Importation de modules externes
-let server = require('./qr');
-let code = require('./pair');
-
-// Augmente le nombre maximum d'auditeurs d'événements
+let server = require('./qr'),
+    code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-
-// Routes pour différents modules
 app.use('/qr', server);
 app.use('/code', code);
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html');
-});
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/hacking.html');
-});
-
-// Middleware pour parser le corps des requêtes
+app.use('/pair',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/hacking.html')
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Lancement du serveur
 app.listen(PORT, () => {
     console.log(`
 Don't Forget To Give Star
 
- Hacking Session Server running on http://localhost:` + PORT);
-});
+ Hacking Session Server running on http://localhost:` + PORT)
+})
 
-module.exports = app;
+module.exports = app
